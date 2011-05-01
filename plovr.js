f318858cff4627949d046b67ca6f7aed22139d83
Dave King <tildedave@gmail.com>
2011-04-30 20:25:51 -0400

    add google closure templates from brianhartsock/closure-files

--- plovr.js
+++ plovr.js
@@ -1,6 +1,9 @@
 // Plovr build
 {
-    "id": "filemanager",
-    "paths": "src/main/js",
-    "inputs": "src/main/js/filemanager.js"
+    "id": "app",
+    "paths": "src/main",
+    "inputs": [ "src/main/js/filemanager.js",
+                "src/main/js/container_table.js",
+                "src/main/soy/container_table_tmpl.soy"   
+              ]
 }
