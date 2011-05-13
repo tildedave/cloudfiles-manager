76cb427cc924cb5adaf7bf2ab041d22f87e475d7
Dave King <tildedave@gmail.com>
2011-05-12 20:54:13 -0400

    More noisy compiler when I mess up.

--- plovr.js
+++ plovr.js
@@ -6,5 +6,16 @@
                 "src/main/js/container_table.js",
                 "src/main/js/container.js",
                 "src/main/soy/cloud_files.soy"
-              ]
+              ],
+    "level": "VERBOSE", 
+    "checks": {
+        // acceptable values are "ERROR", "WARNING", and "OFF" 
+        "deprecated": "ERROR",
+        "checkTypes": "WARNING",
+        "missingProperties": "ERROR",
+        "strictModuleDepCheck": "ERROR",
+        "accessControls": "ERROR",
+        "visibility": "ERROR",
+        "nonStandardJsDocs": "WARNING"
+    }
 }
