e816ae11cdf820605cc993fde94f7190e8fb1f08
Dave King <tildedave@gmail.com>
2011-06-11 15:28:33 -0400

    fire SELECT_CONTAINER in Bootstrap based on hash.

--- src/main/js/Bootstrap.js
+++ src/main/js/Bootstrap.js
@@ -5,5 +5,22 @@ goog.provide('ui.cloud_files.Bootstrap');
 
 ui.cloud_files.Bootstrap.go = function () {
   var broker = new ui.Broker();
-  goog.events.dispatchEvent(broker, ui.Events.SELECT_ALL_CONTAINERS);
+
+  if (window.location.hash) {
+    var container = unescape(window.location.hash.split("#!")[1]);
+    var event = new goog.events.Event(ui.Events.SELECT_CONTAINER,
+                                      { name: container });
+    goog.events.dispatchEvent(broker, event);
+  }
+  else {
+    goog.events.dispatchEvent(broker, ui.Events.SELECT_ALL_CONTAINERS);
+  }
 };
+
+
+
+
+
+
+
+
