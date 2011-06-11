c0e7d12f84bc30f47bcd5b3c745cba833dd0b0a6
Dave King <tildedave@gmail.com>
2011-06-11 15:19:16 -0400

    Fix upload file dialog to work better

--- src/main/js/UploadFileDialog.js
+++ src/main/js/UploadFileDialog.js
@@ -1,6 +1,7 @@
 goog.provide('ui.cloud_files.UploadFileDialog');
 
 goog.require('goog.ui.Dialog');
+goog.require('goog.events');
 
 /**
  * @constructor
@@ -10,19 +11,46 @@ ui.cloud_files.UploadFileDialog = function (container) {
   goog.ui.Dialog.call(this);  
   this.container = container;
 
-  this.setContent("<h2>Upload a File</h2><ul><li>Not implemented yet</li><li>File Upload in JavaScript is annoying (but possible)</li></ul>");
+  var uploadUrl = "/upload/" + container;
+  this.setContent("<h2>Upload a File</h2><ul><li>Not implemented yet</li>" +
+                  "<li>File Upload in JavaScript is annoying (but possible)</li>" +
+                  "</ul>" +
+                  '<form id="fileUploadForm" action="' + uploadUrl + '" ' +
+                       ' method="post" enctype="multipart/form-data">' +
+                  '<input type="file" name="file" id="fileUpload">' +
+                  '</form>');
 
   this.setButtonSet(this._getButtonSet());
+  goog.events.listen(this, goog.ui.Dialog.EventType.SELECT, function (e) {
+    if (e.key === ui.cloud_files.UploadFileDialog.ChooseFile) {
+      // not working now -- need to figure out how we can avoid closing
+      // dialog
+      goog.events.dispatchEvent(goog.dom.getElement("fileUpload"),
+                                goog.events.EventType.CLICK);
+    }
+    if (e.key === ui.cloud_files.UploadFileDialog.UploadFile) {
+      goog.dom.getElement("fileUploadForm").submit();
+    }
+  });
   this.setVisible(true);
 };
 
 goog.inherits(ui.cloud_files.UploadFileDialog, goog.ui.Dialog);
 
+ui.cloud_files.UploadFileDialog.ChooseFile = "ChooseFile";
+ui.cloud_files.UploadFileDialog.UploadFile = "UploadFile";
+ui.cloud_files.UploadFileDialog.Cancel = "Cancel";
+
 ui.cloud_files.UploadFileDialog.prototype._getButtonSet = function () {
   var buttonSet = new goog.ui.Dialog.ButtonSet();
-  buttonSet.addButton({ key: "ChooseFile", caption: "Choose File" }, true);
-  buttonSet.addButton({ key: "UploadFile", caption: "Upload" });
-  buttonSet.addButton({ key: "Cancel", caption: "Cancel" }, false, true);
+  /*
+  buttonSet.addButton({ key: ui.cloud_files.UploadFileDialog.ChooseFile,
+                        caption: "Choose File" }, true);
+   */
+  buttonSet.addButton({ key: ui.cloud_files.UploadFileDialog.UploadFile,
+                        caption: "Upload" });
+  buttonSet.addButton({ key: ui.cloud_files.UploadFileDialog.Cancel,
+                        caption: "Cancel" }, false, true);
 
   return buttonSet;
 }
