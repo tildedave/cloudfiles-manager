8514e376e0feefeff6961fc76e925d556bd3d656
Dave King <tildedave@gmail.com>
2011-06-11 13:36:10 -0400

    make cache/xhr consistent

--- src/main/js/File.js
+++ src/main/js/File.js
@@ -18,6 +18,11 @@ model.cache.FileProvider.prototype.get = function() {
  */
 
 model.xhr.FileProvider = function (container, name) {
-  var url = '/containers/' + container + '/' + name;
-  window.open(url, "Download");
+  this.container = container;
+  this.name = name;
+};
+
+model.xhr.FileProvider.prototype.get = function() {
+  var url = '/containers/' + this.container + '/' + this.name;
+  window.open(url, "Download");    
 };
